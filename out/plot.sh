0a2e38be2f89724eaa620ff1a88d30d8adcb81aa
nave91 <nave30041991@gmail.com>
2013-11-19 12:24:18 -0500

    correcting title in diabetes

--- out/plot.sh
+++ out/plot.sh
@@ -3,7 +3,7 @@ set terminal postscript eps color "Helvetica" 15
 set size 0.5,0.5
 set xtics (0,0.2,0.4,0.6,0.8,1.0)
 set output "mydata.eps"
-set title "weather"
+set title "diabetes"
 set datafile separator ","
 plot 'mydata.dat'  using 2:3 
 EOF
