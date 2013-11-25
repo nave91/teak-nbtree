c364992c98c56312f2476ef83a421e9ec8782cb6
nave91 <nave30041991@gmail.com>
2013-11-25 16:16:06 -0500

    after changing xydia.pdf title

--- out/plot.sh
+++ out/plot.sh
@@ -6,7 +6,7 @@ set terminal postscript eps color "Helvetica" 15
 set size 1,1
 #set xtics (0,0.2,0.4,0.6,0.8,1.0)
 set output "mydata.eps"
-set title "iris"
+set title "diabetes"
 set datafile separator ","
 plot 'mydata.dat'  using 2:3 
 EOF
